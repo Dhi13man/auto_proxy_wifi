86d8d69583ad1c79ca88e763c843b641ce0be026
Dhiman Seal <furyx.ds@gmail.com>
2021-12-20 17:16:37 +0530

    Documentation for system_calls.py

--- src/services/system_calls.py
+++ src/services/system_calls.py
@@ -3,6 +3,21 @@ from typing import Any
 
 
 def exec(cmd: Any, verbose: bool = False) -> int:
+    """
+    Utility function to execute a command in bash and return the exit code.
+
+    PARAMETERS
+    ----------
+    cmd : Any
+        The command to execute.
+    verbose : bool
+        Whether to print the command and output.
+
+    RETURNS
+    -------
+    int
+        The exit code of the command.
+    """
     try:
         return check_call(
             cmd,
@@ -14,8 +29,22 @@ def exec(cmd: Any, verbose: bool = False) -> int:
         if verbose: print(e)
     
 
-
 def exec_output(cmd: Any, verbose: bool = False) -> str:
+    """
+    Utility function to execute a command in bash and return the STDOUT output.
+
+    PARAMETERS
+    ----------
+    cmd : Any
+        The command to execute.
+    verbose : bool
+        Whether to print the command and output.
+    
+    RETURNS
+    -------
+    str
+        The output of the command.
+    """
     try:
         return check_output(
             cmd,
@@ -25,10 +54,19 @@ def exec_output(cmd: Any, verbose: bool = False) -> str:
         if verbose: print(e)
 
 
-
 def powershell_exec_output(cmd: Any) -> str:
     """
-    Utility function to run a command in powershell mode.
+    Utility function to execute a powershell command and return the STDOUT output.
+
+    PARAMETERS
+    ----------
+    cmd : Any
+        The command to execute.
+    
+    RETURNS
+    -------
+    str
+        The output of the command.
     """
     completed = run(
         ["powershell", "-Command", cmd], capture_output=True,
